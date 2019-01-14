a1f23114208e2f872936a55dcea305e1c2c62fd4
Jonah Stegman <jstegman@fred.socs.uoguelph.ca>
2019-01-14 14:48:46 -0500

    added all functions to calendarparser.c

--- assign1/src/CalenderParser.c
+++ assign1/src/CalenderParser.c
@@ -1,1 +1,66 @@
 #include "CalenderParser.h"
+#include "LinkedListAPI.h"
+#include <ctype.h>
+
+ICalErrorCode createCalendar (char* fileName, Calendar** obj){
+	return OK;
+}
+
+void deleteCalendar (Calendar*obj){
+}
+
+char* printCalendar (const Calendar* obj){
+	return "";
+}
+
+char* printError (ICalErrorCode err){
+	return "";
+}
+
+ICalErrorCode writeCalendar (const Calendar* obj){
+	return OK;
+}
+
+void deleteEvent (void* toBeDeleted){
+}
+
+int compareEvents (const void* first, const void* second){
+	return 0;
+}
+
+char* printEvent (void* toBePrinted){
+	return "";
+}
+
+void deleteAlarm (void* toBeDeleted){
+}
+
+int compareAlarms (const void* first, const void* second){
+	return 0;
+}
+
+char* printAlarm (void* toBePrinted){
+	return "";
+}
+
+void deleteProperty (void* toBeDeleted){
+}
+
+int compareProperties (const void* first, const void* second){
+	return 0;
+}
+
+char* printProperty (void* toBePrinted){
+	return "";
+}
+
+void deleteDate (void* toBeDeleted){
+}
+
+int compareDates (const void* first, const void* second){
+	return 0;
+}
+
+char* printDate (void* toBePrinted){
+	return "";
+}
